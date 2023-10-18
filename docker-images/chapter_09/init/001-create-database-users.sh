f716310a839aa2648b463a71c3a7191266a15543
Enrico <sscotty71@gmail.com>
2023-10-18 10:57:34 +0200

    Fixed permission problems on dir: /data/tablespaces/ts_a /data/tablespaces/ts_b /data/tablespaces/ts_c

--- docker-images/chapter_09/init/001-create-database-users.sh
+++ docker-images/chapter_09/init/001-create-database-users.sh
@@ -2,3 +2,6 @@ set -e
 psql -U postgres -c "alter role postgres password 'LearnPostgreSQL'" postgres
 psql -U postgres -c "create role luca with login password  'LearnPostgreSQL'" postgres
 psql -U postgres -c "create role enrico with login password 'LearnPostgreSQL'" postgres
+sudo chown postgres:postgres /data/tablespaces/ts_a 
+sudo chown postgres:postgres /data/tablespaces/ts_b
+sudo chown postgres:postgres /data/tablespaces/ts_c
